8e0f6442f3dac7a604ce1e9fecd65d7441a8441d
RC_Student_lab <RC_Student_lab@10.0.1.93>
2024-09-09 08:23:10 +0000

    create a programme to add two numbers

--- src/main/java/com/mycompany/firstprojectgitup/Firstprojectgitup.java
+++ src/main/java/com/mycompany/firstprojectgitup/Firstprojectgitup.java
@@ -11,6 +11,15 @@ package com.mycompany.firstprojectgitup;
 public class Firstprojectgitup {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int a=5;
+        int b=10;
+        int c= a+b;
+        System.out.println("sum : +c");
+        
+        
+        
     }
+    
+    
+    
 }
